5ae79fa64eb416b7ff6c4fe8ef0fec8af05fe8a2
Alexandru Ardelean <ardeleanalex@gmail.com>
2017-03-11 18:40:44 +0200

    utils/mstpctl-utils-functions.sh: Use $(STATEMENT) instead of legacy `STATEMENT` (SC2006)
    Link: https://github.com/koalaman/shellcheck/wiki/SC2006

--- utils/mstpctl-utils-functions.sh
+++ utils/mstpctl-utils-functions.sh
@@ -2,7 +2,7 @@ mstpctl_parse_ports()
 {
   while [ x"${1+set}" = xset ]
   do
-    # For compatibility: the `all' option.
+    # For compatibility: the 'all' option.
     case $1 in
       all)
 	shift &&
@@ -11,13 +11,13 @@ mstpctl_parse_ports()
     esac
 
     # Primitive state machine...
-    case $1-`uname -s` in
+    case $1-$(uname -s) in
       regex-Linux)
-	all_interfaces=`sed -n 's%^[\ ]*\([^:]*\):.*$%\1%p' < /proc/net/dev`
+	all_interfaces=$(sed -n 's%^[\ ]*\([^:]*\):.*$%\1%p' < /proc/net/dev)
 	shift
 	;;
       regex-*)
-	echo -n "$0 needs to be ported for your `uname -s` system.  " >&2
+	echo -n "$0 needs to be ported for your $(uname -s) system.  " >&2
 	echo "Trying to continue nevertheless." >&2
 	shift
 	;;
@@ -32,10 +32,10 @@ mstpctl_parse_ports()
       regex-set)
 	# The following interface specification are to be parsed as regular
 	# expressions against all interfaces the system provides.
-	i=`egrep "^$1$" << EOAI
+	i=$(egrep "^$1$" << EOAI
 $all_interfaces
 EOAI
-`
+)
 	shift
 	;;
       *-set)
