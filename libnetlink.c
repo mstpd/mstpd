7c351725cb6b4cdfef72797b469e6f951dda2c2d
Vincent Jardin <vjardin@free.fr>
2026-01-16 00:57:23 +0100

    libnetlink: fix socket file descriptor leak on error paths
    The rtnl_open_byproto function was not closing the socket when setsockopt,
    bind, getsockname or address validation failed after socket creation. Use a
    goto pattern to ensure proper cleanup of the file descriptor on all error paths.
    
    Signed-off-by: Vincent Jardin <vjardin@free.fr>

--- libnetlink.c
+++ libnetlink.c
@@ -60,13 +60,13 @@ int rtnl_open_byproto(struct rtnl_handle *rth, unsigned subscriptions,
 	if (setsockopt(rth->fd, SOL_SOCKET, SO_SNDBUF, &sndbuf, sizeof(sndbuf))
 	    < 0) {
 		ERROR("SO_SNDBUF");
-		return -1;
+		goto err_close;
 	}
 
 	if (setsockopt(rth->fd, SOL_SOCKET, SO_RCVBUF, &rcvbuf, sizeof(rcvbuf))
 	    < 0) {
 		ERROR("SO_RCVBUF");
-		return -1;
+		goto err_close;
 	}
 
 	memset(&rth->local, 0, sizeof(rth->local));
@@ -76,24 +76,28 @@ int rtnl_open_byproto(struct rtnl_handle *rth, unsigned subscriptions,
 	if (bind(rth->fd, (struct sockaddr *)&rth->local, sizeof(rth->local)) <
 	    0) {
 		ERROR("Cannot bind netlink socket");
-		return -1;
+		goto err_close;
 	}
 	addr_len = sizeof(rth->local);
 	if (getsockname(rth->fd, (struct sockaddr *)&rth->local, &addr_len) < 0) {
 		ERROR("Cannot getsockname");
-		return -1;
+		goto err_close;
 	}
 	if (addr_len != sizeof(rth->local)) {
 		ERROR("Wrong address length %d\n", addr_len);
-		return -1;
+		goto err_close;
 	}
 	if (rth->local.nl_family != AF_NETLINK) {
 		ERROR("Wrong address family %d\n",
 			rth->local.nl_family);
-		return -1;
+		goto err_close;
 	}
 	rth->seq = time(NULL);
 	return 0;
+
+err_close:
+	close(rth->fd);
+	return -1;
 }
 
 int rtnl_open(struct rtnl_handle *rth, unsigned subscriptions)
