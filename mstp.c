mstpd/mstpd

commit d27d7e93485d881d8ff3a7f85309b545edbe1fc6

treewide: allow room for null-char in strncpy()
strncpy() is not safe as it can put `len` elements of a string and discard
the null-char if there isn't any room left.

An alternative option would be to use `strlcpy()` but that function may not
be portable to all libc implementations.
So, just reduce the `len` by 1 byte to allow room for null-char.

This was reported via string-fortify headers.

Signed-off-by: Alexandru Ardelean <ardeleanalex@gmail.com>

mstp.c
@@ -1615,7 +1615,7 @@ void MSTP_IN_set_mst_config_id(bridge_t *br, __u16 revision, __u8 *name)
         memset(br->MstConfigId.s.configuration_name, 0,
                sizeof(br->MstConfigId.s.configuration_name));
         strncpy((char *)br->MstConfigId.s.configuration_name, (char *)name,
-                sizeof(br->MstConfigId.s.configuration_name));
+                sizeof(br->MstConfigId.s.configuration_name) - 1);
         br_state_machines_begin(br);
     }
 }

netif_utils.c
@@ -60,7 +60,7 @@ int get_hwaddr(char *ifname, __u8 *hwaddr)
 {
     struct ifreq ifr;
     memset(&ifr, 0, sizeof(ifr));
-    strncpy(ifr.ifr_name, ifname, IFNAMSIZ);
+    strncpy(ifr.ifr_name, ifname, IFNAMSIZ - 1);
     if(0 > ioctl(netsock, SIOCGIFHWADDR, &ifr))
     {
         ERROR("%s: get hw address failed: %m", ifname);
@@ -74,7 +74,7 @@ int get_flags(char *ifname)
 {
     struct ifreq ifr;
     memset(&ifr, 0, sizeof(ifr));
-    strncpy(ifr.ifr_name, ifname, IFNAMSIZ);
+    strncpy(ifr.ifr_name, ifname, IFNAMSIZ - 1);
     if(0 > ioctl(netsock, SIOCGIFFLAGS, &ifr))
     {
         ERROR("%s: get interface flags failed: %m", ifname);
@@ -94,7 +94,7 @@ int if_shutdown(char *ifname)
         return -1;
     }
     ifr.ifr_flags &= ~IFF_UP;
-    strncpy(ifr.ifr_name, ifname, IFNAMSIZ);
+    strncpy(ifr.ifr_name, ifname, IFNAMSIZ - 1);
     if(0 > ioctl(netsock, SIOCSIFFLAGS, &ifr))
     {
         ERROR("%s: set if_down flag failed: %m", ifname);
@@ -107,7 +107,7 @@ int ethtool_get_speed_duplex(char *ifname, int *speed, int *duplex)
 {
     struct ifreq ifr;
     memset(&ifr, 0, sizeof(ifr));
-    strncpy(ifr.ifr_name, ifname, IFNAMSIZ);
+    strncpy(ifr.ifr_name, ifname, IFNAMSIZ - 1);
     struct ethtool_cmd ecmd;
 
     ecmd.cmd = ETHTOOL_GSET;

commit 94ef8f366aebe4beb3122f1adf298e554ec80eeb

configuration: Allow settings a configuration name of 32 characters
Since commit d27d7e93485d to remove a compilation warning due to strncpy copy
of the MST region name always set the last characters to NULL.

This chnage is not compliant with the standard
cf 802.1Q-2022 section 13.8 MST Configuration Identifier (MCID):

Each MCID contains the following components:
...
b) The Configuration Name, a variable length text string encoded within a fixed
   field of 32 octets, conforming to IETF RFC 2271’s definition of SnmpAdminString.
   If the Configuration Name is less than 32 characters, the text string should
   be terminated by the NUL character, with the remainder of the 32-octet field
   filled with NUL characters. Otherwise, the text string is encoded with no
   terminating NUL character.

Moreover this change leads to a wrong behavior with some configuration like

mstpctl addbridge br0
mstpctl setmstconfid br0 1 "12345678901234567890123456789012"

With this configuration:
- MST Config name of the MSTP frame with wireshark is
  1234567890123456789012345678901 and not 12345678901234567890123456789012.
  This is the non compliance with the standard and can lead to interoperability issue.
- output of 'mstpctl showmstconfid br0' gives 1234567890123456789012345678901
  and not 12345678901234567890123456789012 for 'Configuration Name' field
- resent of the command 'mstpctl setmstconfid br0 1 "12345678901234567890123456789012"'
  trigs a restart of the bridge state machine whereas no action is expected.

To fix the regression the modification done by commit d27d7e93485d is reverted and
a pragma is added to avoid the GCC warning.

The configuration name is only printed by showmstconfid command and the use of %*s
by this command allows to print a non NUL string without unexpected behavior.

Signed-off-by: Thibaut Collet <thbt.cllt@gmail.com>

mstp.c
@@ -1629,8 +1629,13 @@ void MSTP_IN_set_mst_config_id(bridge_t *br, __u16 revision, __u8 *name)
         assign(br->MstConfigId.s.revision_level, valueRevision);
         memset(br->MstConfigId.s.configuration_name, 0,
                sizeof(br->MstConfigId.s.configuration_name));
+        /* configuration_name can be non NUL terminated cf 802.1Q-2022 section 13.8
+         * copy everything and ignore GCC warning */
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wstringop-truncation"
         strncpy((char *)br->MstConfigId.s.configuration_name, (char *)name,
-                sizeof(br->MstConfigId.s.configuration_name) - 1);
+                sizeof(br->MstConfigId.s.configuration_name));
+#pragma GCC diagnostic pop
         br_state_machines_begin(br);
     }
 }